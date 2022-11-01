12c090935fd4b7e726606f9dc4cea9accd9fb56f
shbeckett <98051500+shbeckett@users.noreply.github.com>
2022-11-01 15:00:18 +0000

    initial commit

---
+++ src/components/games.js
@@ -0,0 +1,38 @@
+export var games = [
+  {
+    id: "1",
+    name: "Olimpia Milano v Barcelona",
+    date: "22 November 2022",
+    description: "All the action from this nailbiter!",
+    poster: "poster1.jpg",
+    src:
+      "https://d8d913s460fub.cloudfront.net/krpanocloud/video/airpano/video-1920x960a-fs.mp4"
+  },
+  {
+    id: "2",
+    name: "Olimpia Milano v Tezenis Verona",
+    date: "25 November 2022",
+    description: "All the action from this nailbiter!",
+    poster: "poster2.jpg",
+    src:
+      "https://d8d913s460fub.cloudfront.net/krpanocloud/video/airpano/video-1920x960a-fs.mp4"
+  },
+  {
+    id: "1",
+    name: "Olimpia Milano v LA Lakers",
+    date: "27 November 2022",
+    description: "All the action from this nailbiter!",
+    poster: "poster3.jpg",
+    src:
+      "https://d8d913s460fub.cloudfront.net/krpanocloud/video/airpano/video-1920x960a-fs.mp4"
+  },
+  {
+    id: "1",
+    name: "Olimpia Milano v Brooklyn Nets",
+    date: "29 November 2022",
+    description: "All the action from this nailbiter!",
+    poster: "poster4.jpg",
+    src:
+      "https://d8d913s460fub.cloudfront.net/krpanocloud/video/airpano/video-1920x960a-fs.mp4"
+  }
+];
